3c46435a35425b894e9807caf9f7875d5f9a4fd5
Biswa <barikbiswa143@gmail.com>
2023-04-22 15:36:55 +0530

    UserSpecific & changes in style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form/Form";
+import { useEffect, useRef, useState } from "react";
+import ProductItem from "./components/Products/ProductItem";
+import CartContext from "./components/Contexts/CartContext";
+import CartModal from "./components/Modal/CartModal";
+import Cart from "./components/Cart/Cart";
+const endpoint = '440c6d62eadb429c864ce9c1fa2fc139' ;
+async function getOrderList(){
+  const response = await fetch(`https://crudcrud.com/api/${endpoint}/mycart`)
+  const data = await response.json()
+  return data ;
+}
+async function createCart(){
+  const response = await fetch(`https://crudcrud.com/api/${endpoint}/mycart`,{
+    method:'POST',
+    headers:{'Content-Type' : 'application/json'},
+    body: JSON.stringify({orderList : []})
+  })
+  const data = await response.json()
+  return data ;
+}
+async function updateCart(id , data){
+ const resopnse = await fetch(`https://crudcrud.com/api/${endpoint}/mycart/${id}`,{
+    method:'PUT',
+    headers:{'Content-Type' : 'application/json'},
+    body: JSON.stringify({orderList : data})
+  })
+  if(!resopnse.ok){
+    console.log('error accoured')
+  }
+  const resdata = await resopnse.json();
+  return resdata ;
+}
+
+
 
 function App() {
+  const crudcrudUpdateID = useRef('')
+  const[productList , setProductList] = useState([]);
+  const [orderList , setOrderList] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const ctxObj = {
+    orderList : orderList ,
+    setOrderList: setOrderList ,
+    productList : productList ,
+    cartVisibility: isModalOpen,
+    setCartVisibility: setIsModalOpen,
+    setProductList : setProductList
+  }
+  useEffect(()=>{
+    getOrderList().then((data)=>{
+      if(data[0]){
+        crudcrudUpdateID.current = data[0]['_id']
+        setOrderList(data[0].orderList)
+        console.log(crudcrudUpdateID)
+      }else{
+        createCart().then(data=>{
+          crudcrudUpdateID.current = data[0]['_id']
+          console.log(crudcrudUpdateID)
+        })
+        console.log('no cart is avilable')
+      }
+      
+      console.log(data)})
+  },[])
+
+  useEffect(()=>{
+    if(orderList.length!==0)
+    updateCart(crudcrudUpdateID.current ,orderList)
+  },[orderList])
+
+
+  const formSubmitHandler = (value) => {
+    setProductList((prevList)=>{
+      return [value , ...prevList]
+    })
+  } 
+  
+  const ShowProducts = productList.map((product)=>{
+   return  <ProductItem key = {product.id} item ={product} ></ProductItem>
+  })
+  let cartItemCount = 0 ;
+  orderList.forEach((item)=>{
+    cartItemCount += (item.quantity) 
+  })
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <CartContext.Provider value = {ctxObj}>
+      {isModalOpen && <CartModal>
+      <Cart></Cart>
+      </CartModal> } 
+      <button style={{float:'right'}} onClick={()=>setIsModalOpen(true)}>{`My Cart ${cartItemCount}`} </button>
+      <Form onSubmit = {formSubmitHandler}></Form>
+      {ShowProducts}
+      </CartContext.Provider>
+      </>
+       
   );
 }
 
